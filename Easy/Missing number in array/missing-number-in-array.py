43bd1650064d44eb92baf4dc6fa6b7b2b009a6fa
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-01-26 20:05:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Missing number in array/missing-number-in-array.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+
+class Solution:
+    def missingNumber(self,array,n):
+        # code here
+        s = sum(array)
+        req = (n*(n+1))//2
+        return req-s
+                
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=Solution().missingNumber(a,n)
+    print(s)
+# } Driver Code Ends
